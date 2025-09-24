2a31900ae664f9773e15d668969f9abe3c6d3ed3
Mohammed-Ben-Cheikh <bencheikh.official@gmail.com>
2025-09-24 11:44:02 +0100

    feat: implement routing and main controller for home page

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
 import path from "path";
-import db from "./config/database.js"
 import { fileURLToPath } from "url";
+import router from "./routes/index.js";
 
 dotenv.config();
 
@@ -10,16 +10,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT ;
-
+const PORT = process.env.PORT;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.render("index", { data: data });
-});
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`app listening on port http://localhost:${PORT}`);
