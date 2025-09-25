541552cc9c928788bb62adcca511c56758352ab9
Mohammed-Ben-Cheikh <bencheikh.official@gmail.com>
2025-09-25 18:11:06 +0100

    feat: add bcryptjs and express-session dependencies; implement auth controller with register, login, and logout functions

---
+++ controllers/authController.js
@@ -0,0 +1,28 @@
+import db from "../config/database.js";
+
+/**
+ * User controller function
+ * @param {import('express').Request} req - Express request object
+ * @param {import('express').Response} res - Express response object
+ */
+export async function register(req, res) {
+
+}
+
+/**
+ * User controller function
+ * @param {import('express').Request} req - Express request object
+ * @param {import('express').Response} res - Express response object
+ */
+export async function login(req, res) {
+
+}
+
+/**
+ * User controller function
+ * @param {import('express').Request} req - Express request object
+ * @param {import('express').Response} res - Express response object
+ */
+export function logout(req, res) {
+  
+}
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 import express from "express";
+import { chatBot } from "../controllers/chatController.js";
 import { home } from "../controllers/mainController.js";
 import { user } from "../controllers/userController.js";
-import { chatBot } from "../controllers/chatController.js";
-
+import { login } from "../controllers/authController.js";
 
 const router = express.Router();
-
+//auth route
+router.get("/auth/login",login)
 router.get("/", home);
 router.get("/user/:id", user);
+
 router.post("/chatbot", chatBot);
 export default router;
