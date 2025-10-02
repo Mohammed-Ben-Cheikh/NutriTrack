e4485574d1e388700df34bd3257fc512fbc8ae4b
medlaq777 <m.laqsoumi@gmail.com>
2025-10-02 21:59:06 +0100

    Create: variables const to call and import functions from models necessary

--- controllers/rapportsController.js
+++ controllers/rapportsController.js
@@ -22,4 +22,22 @@ export async function generateRapports(req, res) {
   const userId = req.session.user.id;
   const meals = await new Meal.getByUerId(userId);
   const userProfile = await new UserProfile.findByUserId(userId);
+
+  if (!userProfile) {
+    return res.status(400).json({
+      error: true,
+      message: "User profile not found. Please complete your profile.",
+    });
+  }
+
+  if (meals.length === 0) {
+    return res.status(400).json({
+      error: true,
+      message: "No meals found. Please log your meals to get recommendations.",
+    });
+  }
+
+  const mealsDiscription = meals.map((meal) => meal.body).join("\n- ");
+  
+  const rapportPrompt =
 }
