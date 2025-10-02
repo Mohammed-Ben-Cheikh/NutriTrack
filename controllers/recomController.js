d10f835e19bb1e5d42040208953b56c9d407fe6c
medlaq777 <m.laqsoumi@gmail.com>
2025-10-02 12:05:14 +0100

    refactoring recommendation system to handle current day meals

--- controllers/recomController.js
+++ controllers/recomController.js
@@ -23,21 +23,23 @@ export async function recomService(req, res) {
 
   if (!userProfile) {
     return res.status(400).json({
-      error: "User profile not found. Please complete your profile.",
+      error: true,
+      message: "User profile not found. Please complete your profile.",
     });
   }
 
   if (meals.length === 0) {
     return res.status(400).json({
-      error: "No meals found. Please log your meals to get recommendations.",
+      error: true,
+      message: "No meals found. Please log your meals to get recommendations.",
     });
   }
 
   const mealsDiscription = meals.map((meal) => meal.body).join("\n- ");
 
   const prompt = `
-Vous êtes un assistant en recommandations nutritionnelles.  
-Votre tâche est d’analyser le profil de l’utilisateur et son historique de repas afin de fournir des **recommandations personnalisées et dynamiques**.  
+Vous êtes un assistant en recommandations nutritionnelles.
+Votre tâche est d’analyser le profil de l’utilisateur et son historique de repas afin de fournir des **recommandations personnalisées et dynamiques**.
 
 ### Profil Utilisateur
 ${JSON.stringify(userProfile, null, 2)}
@@ -46,10 +48,10 @@ ${JSON.stringify(userProfile, null, 2)}
 - ${mealsDiscription}
 
 ### Règles :
-1. **Athlètes** : donner des conseils pour les repas pré/post entraînement, la récupération et l’hydratation.  
-2. **Patients chroniques** (ex. hypertension, diabète, cholestérol) : alerter sur les aliments dangereux (trop salés, trop sucrés, trop gras) et proposer des alternatives sûres.  
-3. **Perte / Prise de poids** : évaluer le bilan calorique et proposer des ajustements pour le prochain repas (réduire/augmenter les glucides, protéines ou lipides).  
-4. Les recommandations doivent toujours être claires, actionnables et adaptées à l’utilisateur.  
+1. **Athlètes** : donner des conseils pour les repas pré/post entraînement, la récupération et l’hydratation.
+2. **Patients chroniques** (ex. hypertension, diabète, cholestérol) : alerter sur les aliments dangereux (trop salés, trop sucrés, trop gras) et proposer des alternatives sûres.
+3. **Perte / Prise de poids** : évaluer le bilan calorique et proposer des ajustements pour le prochain repas (réduire/augmenter les glucides, protéines ou lipides).
+4. Les recommandations doivent toujours être claires, actionnables et adaptées à l’utilisateur.
 
 À partir de ces données, fournissez **des recommandations précises et adaptées** pour cet utilisateur donner un forma markdown avec des stryle pro.
 `;
